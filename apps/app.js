79e53444206b12ba0f044e16b051d5d52c18decc
Matthew Rattigan <72994838+MattRattigan@users.noreply.github.com>
2022-03-20 14:50:43 -0400

    link files to index.html

---
+++ apps/app.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  var password = document.getElementById('floatingPassword').value;
+  if (password == 1234) {
+    location.replace("./sign-in/clock-out.html")
+  } else {
+    alert("Please enter correct password")
+  }
+}
+
+function aFunction() {
+  var val = document.getElementById('text1').value;
+  var val2 = document.getElementById('text2').value;
+  document.getElementById('textOutput').innerHTML = val;
+  document.getElementById('textOutput2').innerHTML = val2;
+}
+
+function save() {
+  var text_box_one = document.getElementById('text1').value;
+}
---
+++ apps/time.js
@@ -0,0 +1,97 @@
+function startTime() {
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  var date = new Date();
+  var day = date.getDate();
+  var nameDay = weekdays[date.getDay()];
+  var nameMonth = month[date.getMonth()];
+
+  document.getElementById('time').innerHTML = nameDay + ' ' + nameMonth + ' ' + day + ':';
+  clock();
+  startTime2();
+  clock2();
+}
+
+function clock() {
+  var watch = new Date();
+  document.getElementById('hours').innerHTML =
+    watch.toLocaleString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    })
+}
+
+function startTime2() {
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  var date = new Date();
+  var day = date.getDay();
+  var nameDay = weekdays[date.getDay()];
+  var nameMonth = month[date.getMonth()];
+  var year = date.getFullYear();
+  document.getElementById('time2').innerHTML = nameMonth +', '  + nameDay + ' ' + year;
+}
+
+function clock2() {
+  var watch = new Date();
+  document.getElementById('hours2').innerHTML =
+    watch.toLocaleString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    })
+}
+
+function leaveFunc() {
+  var time = new Date();
+  var val = "Called in Sick: @";
+  var newVal = val + " " + time;
+  document.getElementById("outStatus").innerHTML = newVal;
+  return leaveFunc();
+}
+
+function leaveFunc2() {
+  var time = new Date();
+  var val = "On Vacation: @";
+  var newVal = val + " " + time;
+  document.getElementById("outStatus").innerHTML = newVal;
+}
+
+function selectEmployee() {
+  var val = document.getElementById("ex1").value;
+  var newVal = "Employee:" + " " + val;
+  document.getElementById("outStatusEmployee").innerHTML = newVal;
+}
+
+function selectEmployee2() {
+  var val = document.getElementById("ex2").value;
+  var newVal = "Employee:" + " " + val;
+  document.getElementById("outStatusEmployee").innerHTML = newVal;
+}
+
+function selectEmployee3() {
+  var val = document.getElementById("ex3").value;
+  var newVal = "Employee:" + " " + val;
+  document.getElementById("outStatusEmployee").innerHTML = newVal;
+}
+
+function selectLocation() {
+  var val = document.getElementById("lx1").value;
+  var newVal = "Location:" + " " + val;
+  document.getElementById("outLocation").innerHTML = newVal;
+}
+
+function selectLocation2() {
+  var val = document.getElementById("lx2").value;
+  var newVal = "Location:" + " " + val;
+  document.getElementById("outLocation").innerHTML = newVal;
+}
+
+function selectLocation3() {
+  var val = document.getElementById("lx3").value;
+  var newVal = "Location:" + " " + val;
+  document.getElementById("outLocation").innerHTML = newVal;
+}
